b4a7b5ee17388915f8fc5b79f5466458ff7157fe
wgs <18258548+room1401@users.noreply.github.com>
2023-08-01 20:32:55 +0800

    Add files via upload

---
+++ fcc-py Budget App/budget.py
@@ -0,0 +1,74 @@
+class Category:
+
+  def __init__(self, name):
+    self.name = name
+    self.ledger = []
+
+  def __repr__(self):
+    record = self.name.center(30, '*') + '\n'
+    for i in self.ledger:
+      record += i['description'][:23].ljust(23) + f'{i["amount"]:.2f}'.rjust(
+        7) + '\n'
+    record += f'Total: {self.get_balance():.02f}'
+    return record
+
+  def deposit(self, amt, desc=''):
+    self.ledger.append({'amount': amt, 'description': desc})
+
+  def get_balance(self):
+    balance = 0
+    if not self.ledger:
+      return balance
+    for i in range(len(self.ledger)):
+      balance += self.ledger[i]['amount']
+    return balance
+
+  def check_funds(self, amt):
+    if amt > self.get_balance():
+      return False
+    return True
+
+  def withdraw(self, amt, desc=''):
+    if self.check_funds(amt):
+      self.ledger.append({'amount': -amt, 'description': desc})
+      return True
+    return False
+
+  def transfer(self, amt, cat):
+    if self.withdraw(amt, f'Transfer to {cat.name}'):
+      cat.deposit(amt, f'Transfer from {self.name}')
+      return True
+    return False
+
+  def get_expenseR(self):
+    ratio = 1 - self.get_balance() / self.ledger[0]['amount']
+    return int(ratio * 10)
+
+
+def create_spend_chart(request):
+
+  def justify(strList):
+    maxLen = len(max(strList, key=len))
+    return [x.ljust(maxLen) for x in strList]
+
+  def joinFlip(header, content):
+    for item in content:
+      for i in range(len(item)):
+        header[i] += item[i].center(3)
+    return header
+
+  title = 'Percentage spent by category'
+
+  meter = [(str(x * 10) + '|').rjust(4) for x in range(0, 11)]
+  expense = ['o' * (x.get_expenseR() + 1) for x in request]
+  expense = justify(expense)
+  chart = '\n'.join(joinFlip(meter, expense)[::-1])
+
+  xAxis = ' ' * 4 + '-' * (3 * len(request) + 1)
+
+  label = [x.name for x in request]
+  label = justify(label)
+  padding = [' ' * 4 for x in range(len(label[0]))]
+  vLabel = '\n'.join(joinFlip(padding, label))
+
+  return '\n'.join([title, chart, xAxis, vLabel])
