2c2507aab546520cdd19a746da5a0a99fcb837fd
wgs <18258548+room1401@users.noreply.github.com>
2023-08-09 19:21:49 +0800

    v1.1 code cleaned

--- fcc-py Arithmetic Formatter/arithmetic_arranger.py
+++ fcc-py Arithmetic Formatter/arithmetic_arranger.py
@@ -18,16 +18,15 @@ def arithmetic_arranger(problems, solve = False):
   if checkError(problems):
     return '\n'.join(checkError(problems))
 
-  ln1, ln2, ln3 = "", "\n", "\n"
-  ln4 = "\n" if solve else ""
-  for i in range(len(problems)):
-    margin = len(max(problems[i].split(), key=len)) + 2
-    padding = "" if i == len(problems) - 1 else " " * 4
-    num1, optr, num2 = problems[i].split()
-    ln1 += num1.rjust(margin) + padding
-    ln2 += optr + num2.rjust(margin - 1) + padding
-    ln3 += "-" * margin + padding
+  output = ['', '', '', ''] if solve else ['', '', '']
+  for prob in problems:
+    margin = len(max(prob.split(), key=len)) + 2
+    padding = '' if prob == problems[-1] else ' ' * 4
+    num1, optr, num2 = prob.split()
+    output[0] += num1.rjust(margin) + padding
+    output[1] += optr + num2.rjust(margin - 1) + padding
+    output[2] += '-' * margin + padding
     if solve:
-      ln4 += str(int(num1) + int(optr + num2)).rjust(margin) + padding
+      output[3] += str(int(num1) + int(optr + num2)).rjust(margin) + padding
   
-  return ln1 + ln2 + ln3 + ln4
+  return '\n'.join(output)
