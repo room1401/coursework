3b4abda513b611864fb68ccd8f368ada9c471b85
wgs <18258548+room1401@users.noreply.github.com>
2023-08-13 04:31:01 +0800

    v1.0

--- fcc-py DemoDataAnalyzer/demographic_data_analyzer.py
+++ fcc-py DemoDataAnalyzer/demographic_data_analyzer.py
@@ -47,20 +47,12 @@ def calculate_demographic_data(print_data=True):
     print("Number of each race:\n", race_count)
     print("Average age of men:", average_age_men)
     print(f"Percentage with Bachelors degrees: {percentage_bachelors}%")
-    print(
-      f"Percentage with higher education that earn >50K: {higher_education_rich}%"
-    )
-    print(
-      f"Percentage without higher education that earn >50K: {lower_education_rich}%"
-    )
+    print(f"Percentage with higher education that earn >50K: {higher_education_rich}%")
+    print(f"Percentage without higher education that earn >50K: {lower_education_rich}%")
     print(f"Min work time: {min_work_hours} hours/week")
-    print(
-      f"Percentage of rich among those who work fewest hours: {rich_percentage}%"
-    )
+    print(f"Percentage of rich among those who work fewest hours: {rich_percentage}%")
     print("Country with highest percentage of rich:", highest_earning_country)
-    print(
-      f"Highest percentage of rich people in country: {highest_earning_country_percentage}%"
-    )
+    print(f"Highest percentage of rich people in country: {highest_earning_country_percentage}%")
     print("Top occupations in India:", top_IN_occupation)
 
   return {
